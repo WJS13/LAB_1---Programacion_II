52458a2f1adce0aa2441fd6821ab512437f633b5
Gustavo <perezgustavo2103@gmail.com>
2022-10-28 01:29:58 -0500

    modificacion

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 const {datos} = require('./data')
 
+app.use(express.json())//requerido para trabajar con json
+
 app.get('/', (req, res)=>{
     res.send('<h1>Página Principal</h1><a href="/api/datos">DATOS</a>')
 })
@@ -10,8 +12,8 @@ app.get('/', (req, res)=>{
 app.get('/api/datos', (req, res)=>{
     //res.json(datos)
     const simpleData = datos.map((dato)=>{
-        const{id, name} = dato
-        return{id, name}
+        const{id, name, apellidos, dni, edad} = dato
+        return{id, name, apellidos, dni, edad}
     })
     res.send(simpleData)
 })
@@ -21,18 +23,17 @@ app.post('/api/datos',(req,res)=>{
     if(!name){
         return res
             .status(400)
-            .json({sucess:false,msg:"Proveer nombre"})
+            .json({success:false,msg:"Proveer nombre"})
     }
-    res.status(201).send({sucess:true,data:[...datos,name]})
+    res.status(201).send({success:true,data:[...datos,name]})
+})
 
 app.put('/api/datos/:id', (req,res) => {
     const{id}=req.params
     const{name}=req.body
     const data= datos.find((data) => data.id === Number(id))
     if(!data){
-        return res
-        .status(404)
-        .json({success:false, msg: 'no persona con id: ${id}'})
+        return res.status(404).json({success:false, msg: 'no persona con id: ${id}'})
     }
     const newdato = datos.map(data => {
         if (data.id ===  Number(id)) {
@@ -41,21 +42,19 @@ app.put('/api/datos/:id', (req,res) => {
         return data
         })
         res.status(201).send({success: true, data: newdato})
-    })
+})
 
 app.delete('/api/datos/:id', (req,res) => {
     const{id}=req.params
     const data= datos.find((data) => data.id === Number(id))
-    if(!cata){
+    if(!data){
         return res
         .status(404)
         .json({success:false, msg: 'no persona con id: ${id}'})
     }
     const newdatos = datos.filter(data => data.id !==  Number(id))
     return res.status(200).send({success: true, data: newdatos})
-    })    
-
-})
+})    
 
 app.listen(5500,()=>{
     console.log('Server esta en el puerto 5500...');
