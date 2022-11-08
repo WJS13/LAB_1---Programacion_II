8bcc90d8ccde731e54bc8534091785b2f1f7df6d
Gustavo <perezgustavo2103@gmail.com>
2022-11-07 23:18:10 -0500

    Mejoras de la aplicación

--- app.js
+++ app.js
@@ -1,61 +1,79 @@
 const express = require('express')
 const app = express()
 
-const {datos} = require('./data')
+const {users} = require('./data')
 
-app.use(express.json())//requerido para trabajar con json
+app.use(express.json()) //requerido para trabajar con json
 
 app.get('/', (req, res)=>{
-    res.send('<h1>Página Principal</h1><a href="/api/datos">DATOS</a>')
+    res.send('<h1>Página Principal</h1><a href="/api/users">USUARIOS</a>')
 })
 
-app.get('/api/datos', (req, res)=>{
-    //res.json(datos)
-    const simpleData = datos.map((dato)=>{
-        const{id, name, apellido, dni, edad} = dato
-        return{id, name, apellido, dni, edad}
+app.get('/api/users', (req, res)=>{
+    // res.json(users)
+    const dataUsers = users.map((dato)=>{
+        const {id, name, surname, dni, age} = dato
+        return {id, name, surname, dni, age}
     })
-    res.send(simpleData)
+    res.send(dataUsers)
 })
 
-app.post('/api/datos',(req,res)=>{
-    const {name} = req.body
-    if(!name){
+app.post('/api/users',(req,res)=>{
+    const {id, name, surname, dni, age} = req.body
+    const test_id= users.find((data) => data.id === id)
+    if(test_id){
         return res
             .status(400)
-            .json({success:false,msg:"Proveer nombre"})
+            .json({success:false, msg:`Ya exite una persona con un id igual a ${id}`})
     }
-    res.status(201).send({success:true,data:[...datos,name]})
+    if(!id || !name || !surname || !dni || !age){
+        return res
+            .status(400)
+            .json({success:false,msg:"Faltan datos. Ingrese correctamente lo siguiente: id, name, surname, dni y age."})
+    }
+
+    res.status(201).send({success:true,data:[...users,{id, name, surname, dni, age}]})
 })
 
-app.put('/api/datos/:id', (req,res) => {
+app.put('/api/users/:id', (req,res) => {
     const{id}=req.params
-    const{name}=req.body
-    const data= datos.find((data) => data.id === Number(id))
+    const{name, surname, dni, age}=req.body
+    const data= users.find((data) => data.id === Number(id))
     if(!data){
-        return res.status(404).json({success:false, msg: 'no persona con id: ${id}'})
+        return res.status(404).json({success:false, msg: `no persona con id: ${id}`})
     }
-    const newdato = datos.map(data => {
+    const newdato = users.map((data) => {
         if (data.id ===  Number(id)) {
-            data.name = name
+            if (name != null){
+            data.name = name   
+            }
+            if (surname != null){
+            data.surname = surname   
+            }
+            if (dni != null){
+            data.dni = dni  
+            }
+            if (age != null){
+            data.age = age   
+            }
         }
         return data
         })
-        res.status(201).send({success: true, data: newdato})
+    res.status(201).send({success: true, data: newdato})
 })
 
-app.delete('/api/datos/:id', (req,res) => {
+app.delete('/api/users/:id', (req,res) => {
     const{id}=req.params
-    const data= datos.find((data) => data.id === Number(id))
+    const data= users.find((data) => data.id === Number(id))
     if(!data){
         return res
         .status(404)
-        .json({success:false, msg: 'no persona con id: ${id}'})
+        .json({success:false, msg: `no persona con id: ${id}`})
     }
-    const newdatos = datos.filter(data => data.id !==  Number(id))
+    const newdatos = users.filter(data => data.id !==  Number(id))
     return res.status(200).send({success: true, data: newdatos})
 })    
 
-app.listen(5500,()=>{
-    console.log('Server esta en el puerto 5500...');
+app.listen(4000,()=>{
+    console.log('Server esta en el puerto 4000...');
 })
